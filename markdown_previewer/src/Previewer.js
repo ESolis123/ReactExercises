ca9c93e913ff6b3f3e7848f8a806e958c684bd39
Enrique Solís <jose11@windowslive.com>
2025-05-23 19:37:33 -0400

    Asignando estilos al previewer

--- markdown_previewer/src/Previewer.js
+++ markdown_previewer/src/Previewer.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { act } from "react";
 import './App.css'
+
 class Previewer extends React.Component{
 
     constructor(props){
@@ -10,7 +11,7 @@ class Previewer extends React.Component{
 
         this.checkText = this.checkText.bind(this);
         this.convert = this.convert.bind(this);
-
+        this.actualParent = null;
     }
 
     checkText(){
@@ -19,11 +20,24 @@ class Previewer extends React.Component{
         let encabezado = [];
 
         for (let line of text){
-            if(line !== '')
-                encabezado.push(this.convert(line));
+            if(line !== ''){
+                let element = this.convert(line)
+
+                if(this.actualParent === null)
+                    encabezado.push(element);
+                else
+                    this.actualParent.push(element);
+
+                if(element.type === "h1" || element.type === "h2"){
+                    encabezado.push(<hr className={element.type} />)
+                }
+            }
+
         }
 
         this.content.push(encabezado);
+        if(this.actualParent !== null)
+           this.content.push(<div className="code">{this.actualParent}</div>);
         console.log(this.content)
     }
 
@@ -39,14 +53,40 @@ class Previewer extends React.Component{
     convert(text){
         let encabezado = '';
 
-        if(text.match('^#{1}')){
+        if(text.match('^#{2}')){
+            if(this.actualParent === null)
+                encabezado = <h2>{text}</h2>
+            else
+                encabezado = <p>{text}</p>
+        }
 
-            encabezado = <h1 key={this.lastIndex}>{text}</h1>
+        else if(text.match('^#{1}')){
+             if(this.actualParent === null)
+                encabezado = <h1>{text}</h1>
+            else
+                encabezado = <p>{text}</p>
+        }
+
+        else if(text.match('^(\/){2}')){
+            encabezado = <p className="comment">{text}</p>
+        }
+
+        else if(text.match('```')){
+            if(this.actualParent === null){
+                this.actualParent = [];
+            }
+
+            else{
+                this.content.push(<div className="code">{this.actualParent}</div>);
+                this.actualParent = null;
+            }
+
         }
 
         else{
-            encabezado = <p key={this.lastIndex}>{text}</p>;
+            encabezado =  <p>{text}</p>
         }
+
         this.lastIndex++;
         return encabezado;
     }
